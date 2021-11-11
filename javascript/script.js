fd37203f17214f428d1c5c9c1160dbda9f1643d9
FidelisMomanyi <fidelismomanyi001@gmail.com>
2021-11-12 01:20:29 +0300

    Edited script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,7 @@
 let daysOfTheWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
 let namesOfMale = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
 let namesOfFemale = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama",]
+function validateForm() {
+    let year = document.form["akanName"]["Year"].info;
+    let month = document.form["akanName"]["Month"]
+}
