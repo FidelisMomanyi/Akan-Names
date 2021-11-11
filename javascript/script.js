0edbe31f32d2b378267036441f93ded56ba41769
FidelisMomanyi <fidelismomanyi001@gmail.com>
2021-11-12 01:51:29 +0300

    Edited script.js

--- javascript/script.js
+++ javascript/script.js
@@ -3,5 +3,13 @@ let namesOfMale = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
 let namesOfFemale = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama",]
 function validateForm() {
     let year = document.form["akanName"]["Year"].info;
-    let month = document.form["akanName"]["Month"]
-}
+    let month = document.form["akanName"]["Month"].info;
+    let day = document.form["akanName"]["Day"].info;
+    if (Year == "" || Month == "" || Day == ""){
+        console.log()
+        alert("Please fill all the form details correctly");
+        return false;
+    }else{
+        getUserInput()
+    }
+    }
