404a7f4c1d281d717376d6034cedfa2480dec701
FidelisMomanyi <fidelismomanyi001@gmail.com>
2021-11-12 01:08:12 +0300

    Added data to script.js

---
+++ javascript/script.js
@@ -0,0 +1,3 @@
+let daysOfTheWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+let namesOfMale = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+let namesOfFemale = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama",]
